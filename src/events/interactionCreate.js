347d5586bf68f9749c74d07626f74a9da5055e3f
ZackDevs <jorgerib032006@gmail.com>
2023-09-22 17:47:42 +0100

    Forgot to add this to the last commit

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -3,23 +3,37 @@ const { errEmbed } = require("../util/easyEmbed")
 module.exports = {
     name: Events.InteractionCreate,
     /**
-     * 
      * @param {ChatInputCommandInteraction} interaction 
-     * @returns {Promise}
      */
     async run(interaction) {
-        if (!interaction.isChatInputCommand()) return;
-            const date = Date.now()
-
-            const command = interaction.client.commands.get(interaction.commandName);
-
-            if (!command) {
-                console.error(`No command matching ${interaction.commandName} was found.`);
-                return;
-            }
-
+        const date = Date.now()
+        const command = interaction.client.commands.get(interaction.commandName);
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+        if (!interaction.isChatInputCommand() && !interaction.isAutocomplete()) {
             try {
-                const options = interaction.options._hoistedOptions.reduce((acc, cur) => ({...acc, [cur.name]: cur.value}), {})
+                await command.execute({ interaction, date, errEmbed });
+            } catch (error) {
+                console.error(error);
+                if (interaction.replied || interaction.deferred) {
+                    await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+                } else {
+                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+                }
+            }
+            return
+        }
+        if (interaction.isAutocomplete()) {
+            try {
+                return await command.autocomplete(interaction);
+            } catch (error) {
+                return console.error(error);
+            }
+        }
+            try {
+                const options = interaction.options._hoistedOptions.reduce((acc, cur) => ({...acc, [cur.name]: cur.type === 11 ? cur.attachment : cur.value }), {})
                 await command.execute({ interaction, options, date, errEmbed });
             } catch (error) {
                 console.error(error);
