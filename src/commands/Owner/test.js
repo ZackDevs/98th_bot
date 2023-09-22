03786d68ebbdcf922b27b5dcd4ad01d7ca3b4c21
ZackDevs <jorgerib032006@gmail.com>
2023-09-22 17:27:06 +0100

    I'm stupid

--- src/commands/Owner/test.js
+++ src/commands/Owner/test.js
@@ -1,14 +1,14 @@
-const { SlashCommandBuilder, PermissionFlagsBits, BaseInteraction} = require("discord.js")
+const { SlashCommandBuilder, PermissionFlagsBits, ChatInputCommandInteraction} = require("discord.js")
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('omg')
 		.setDescription('Replies with Pong!')
         .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
     /**
-     * 
-     * @param {BaseInteraction} interaction 
+     * @param {Object} obj
+     * @param {ChatInputCommandInteraction} obj.interaction 
      */
 	async execute({ interaction }) {
-        interaction.send("Hello")
+        interaction.reply("Hello")
 	}, 
 };
