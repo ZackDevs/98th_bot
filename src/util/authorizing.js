e2200aa7bcf002e804abf637a83d1d3a1c2b6eaf
ZackDevs <jorgerib032006@gmail.com>
2023-09-28 15:07:09 +0100

    Issue with directions

--- src/util/authorizing.js
+++ src/util/authorizing.js
@@ -7,7 +7,8 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+
+const TOKEN_PATH = path.join(__dirname, "../../token.json")
 async function authorize() {
   const auth = new google.auth.GoogleAuth({
     keyFile: TOKEN_PATH, //the key file
